4140b2d2fe5985d98d112c577b285b9ebfacb4ed
MaxX <maxx-f@mail.ru>
2022-08-05 11:51:23 +0300

    Add Task003 задачи

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,20 +1,27 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N),
 // а на выходе показывает все целые числа в промежутке от -N до N.
 
-Console.WriteLine("Введите целое положительное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int count = number;
-Console.Write(number);
-while(count > -number)
-{
-    number = number - 1;
-    Console.Write(number );
-}
+//Console.WriteLine("Введите целое положительное число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int count = number;
+//Console.Write(number);
+//while(count > -number)
+//{
+//    number = number - 1;
+//    Console.Write(number );
+//}
 
 // Напишитее программу, которая принимает на вход трехзначное число,
 // и на выходе показывает последнюю цифру.
 
 Console.WriteLine("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 && num < 1000)
+{
 int digit = num % 10;
 Console.WriteLine($"Последняя цифра числа {num} равна {digit}");
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
