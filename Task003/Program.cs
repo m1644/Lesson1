6a4839fb56b0b0036e737192b3a9d3720fb0d9cd
MaxX <maxx-f@mail.ru>
2022-08-05 11:43:27 +0300

    Add Task003

---
+++ Task003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+Console.Write(number);
+while(count > -number)
+{
+    number = number - 1;
+    Console.Write(number );
+}
+
+// Напишитее программу, которая принимает на вход трехзначное число,
+// и на выходе показывает последнюю цифру.
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit = num % 10;
+Console.WriteLine($"Последняя цифра числа {num} равна {digit}");
